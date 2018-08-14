a4904ed8f10bcc57e48cc7ff24cdd0575a23e63a
Adam Bravo <zcm@interia.pl>
2018-08-14 23:07:40 +0200

    Init Project

---
+++ src/main/java/com/github/zcmee/godmodehibernate/GodmodehibernateApplication.java
@@ -0,0 +1,12 @@
+package com.github.zcmee.godmodehibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodmodehibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodmodehibernateApplication.class, args);
+	}
+}
